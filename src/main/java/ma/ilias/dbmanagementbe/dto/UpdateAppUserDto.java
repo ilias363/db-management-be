6d2de6693a394973e8985411cf7f7af2c0f2398c
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-07-03 21:51:42 +0100

    Create update user dto

---
+++ src/main/java/ma/ilias/dbmanagementbe/dto/UpdateAppUserDto.java
@@ -0,0 +1,32 @@
+package ma.ilias.dbmanagementbe.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+import ma.ilias.dbmanagementbe.validation.ExistingRoles;
+import ma.ilias.dbmanagementbe.validation.UniqueUsername;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class UpdateAppUserDto {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    @UniqueUsername
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotNull(message = "Active status is required")
+    private Boolean active;
+
+    @NotEmpty(message = "At least one role is required")
+    @ExistingRoles
+    @ToString.Exclude
+    private Collection<Long> roles = new ArrayList<>();
+}
