f862364a17f8bf8363834c8a9c18fb3cdff00955
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-07-03 13:40:43 +0100

    Testing 2 datasources conn

--- src/main/java/ma/ilias/dbmanagementbe/controller/TestController.java
+++ src/main/java/ma/ilias/dbmanagementbe/controller/TestController.java
@@ -4,6 +4,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.Getter;
 import lombok.Setter;
+import ma.ilias.dbmanagementbe.dao.entities.AppUser;
+import ma.ilias.dbmanagementbe.dao.repositories.AppUserRepository;
+import ma.ilias.dbmanagementbe.service.TestTargetDbService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,19 +14,31 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class TestController {
 
     private final AuthenticationManager authenticationManager;
+    private final TestTargetDbService targetDbService;
+    private final AppUserRepository appUserRepository;
 
-    public TestController(AuthenticationManager authenticationManager) {
+    public TestController(
+            AuthenticationManager authenticationManager,
+            TestTargetDbService targetDbService,
+            AppUserRepository appUserRepository
+    ) {
         this.authenticationManager = authenticationManager;
+        this.targetDbService = targetDbService;
+        this.appUserRepository = appUserRepository;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest,
-                                        HttpServletRequest request) {
+    public ResponseEntity<String> login(
+            @RequestBody LoginRequest loginRequest,
+            HttpServletRequest request
+    ) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         loginRequest.getUsername(),
@@ -56,6 +71,16 @@ public class TestController {
     @GetMapping("/public")
     public ResponseEntity<String> publicEndpoint() {
         return ResponseEntity.ok("Public API Endpoint");
+    }
+
+    @GetMapping("/databases")
+    public List<String> listTables() {
+        return targetDbService.getDatabases();
+    }
+
+    @GetMapping("/users")
+    public List<AppUser> listUsers() {
+        return appUserRepository.findAll();
     }
 }
 
