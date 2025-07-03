e82eb8db18e801225d23db26712f161499a83b8f
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-07-03 11:38:36 +0100

    Initial commit

---
+++ src/main/java/ma/ilias/dbmanagementbe/DbManagementBeApplication.java
@@ -0,0 +1,13 @@
+package ma.ilias.dbmanagementbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbManagementBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbManagementBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/ilias/dbmanagementbe/DbManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ilias.dbmanagementbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbManagementBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
