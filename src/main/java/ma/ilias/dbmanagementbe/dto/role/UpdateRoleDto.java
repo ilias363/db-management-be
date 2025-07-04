cab2a387422648b2b98b2c33ff0221a1404589a1
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-07-04 15:26:00 +0100

    Add update role dto

---
+++ src/main/java/ma/ilias/dbmanagementbe/dto/role/UpdateRoleDto.java
@@ -0,0 +1,26 @@
+package ma.ilias.dbmanagementbe.dto.role;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+import ma.ilias.dbmanagementbe.validation.ExistingPermissions;
+import ma.ilias.dbmanagementbe.validation.UniqueRoleName;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UpdateRoleDto {
+    @NotBlank(message = "Role name is required")
+    @UniqueRoleName
+    private String name; // ADMIN, VIEWER, CUSTOM_ROLE_NAME
+
+    private String description;
+
+    @NotEmpty(message = "At least one permission is required")
+    @ExistingPermissions
+    private Collection<Long> permissions = new ArrayList<>();
+}
