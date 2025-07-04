a61609403a25b4d7a8ffe389e3abd7f946c1374a
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-07-04 15:25:34 +0100

    Update validation for existing permissions, and roles to handle dtos

--- src/main/java/ma/ilias/dbmanagementbe/validation/ExistingRolesValidator.java
+++ src/main/java/ma/ilias/dbmanagementbe/validation/ExistingRolesValidator.java
@@ -3,20 +3,36 @@ package ma.ilias.dbmanagementbe.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import ma.ilias.dbmanagementbe.dao.repositories.RoleRepository;
+import ma.ilias.dbmanagementbe.dto.role.RoleDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Collection;
 
-public class ExistingRolesValidator implements ConstraintValidator<ExistingRoles, Collection<Long>> {
+public class ExistingRolesValidator implements ConstraintValidator<ExistingRoles, Collection<?>> {
 
     @Autowired
     private RoleRepository roleRepository;
 
     @Override
-    public boolean isValid(Collection<Long> roleIds, ConstraintValidatorContext context) {
-        if (roleIds == null || roleIds.isEmpty()) {
-            return true; // Handled by @NotEmpty
+    public boolean isValid(Collection<?> roles, ConstraintValidatorContext context) {
+        if (roles == null || roles.isEmpty()) {
+            return true;
         }
-        return roleIds.stream().allMatch(roleRepository::existsById);
+
+        for (Object role : roles) {
+            Long roleId;
+            if (role instanceof Long) {
+                roleId = (Long) role;
+            } else if (role instanceof RoleDto) {
+                roleId = ((RoleDto) role).getId();
+            } else {
+                return false;
+            }
+
+            if (!roleRepository.existsById(roleId)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
