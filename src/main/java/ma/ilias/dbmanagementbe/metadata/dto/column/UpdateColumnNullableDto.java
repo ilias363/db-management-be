f5f4c327289025782f11b3208bb0de28ae9f1fc6
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-07-10 12:49:13 +0100

    Add missing attributes to unique/nullable column dto update

--- src/main/java/ma/ilias/dbmanagementbe/metadata/dto/column/UpdateColumnNullableDto.java
+++ src/main/java/ma/ilias/dbmanagementbe/metadata/dto/column/UpdateColumnNullableDto.java
@@ -1,5 +1,6 @@
 package ma.ilias.dbmanagementbe.metadata.dto.column;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import ma.ilias.dbmanagementbe.validation.NotNullableWithDefault;
@@ -7,6 +8,15 @@ import ma.ilias.dbmanagementbe.validation.NotNullableWithDefault;
 @Data
 @NotNullableWithDefault
 public class UpdateColumnNullableDto {
+    @NotBlank(message = "Schema name cannot be blank")
+    private String schemaName;
+
+    @NotBlank(message = "Table name cannot be blank")
+    private String tableName;
+
+    @NotBlank(message = "Column name cannot be blank")
+    private String columnName;
+
     @NotNull(message = "The isNullable field cannot be null.")
     private Boolean isNullable;
 
