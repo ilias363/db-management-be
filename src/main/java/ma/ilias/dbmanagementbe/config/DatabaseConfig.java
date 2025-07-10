84791ab7d1207b40ba803f217f511a84d81c3cb7
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-07-10 13:40:28 +0100

    Add a jdbc template bean

--- src/main/java/ma/ilias/dbmanagementbe/config/DatabaseConfig.java
+++ src/main/java/ma/ilias/dbmanagementbe/config/DatabaseConfig.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
@@ -23,5 +26,10 @@ public class DatabaseConfig {
     public HikariDataSource targetDataSource(
             @Qualifier("targetDataSourceProperties") DataSourceProperties targetDataSourceProperties) {
         return targetDataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    public JdbcTemplate targetJdbcTemplate(@Qualifier("targetDatasource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
