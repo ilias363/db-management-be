b1c9fd72e9298c8a9e451ac4530701822c25089a
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-07-17 23:59:23 +0100

    Add update column unique with validation

---
+++ src/main/java/ma/ilias/dbmanagementbe/metadata/dto/column/update/UpdateColumnUniqueDto.java
@@ -0,0 +1,25 @@
+package ma.ilias.dbmanagementbe.metadata.dto.column.update;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import ma.ilias.dbmanagementbe.metadata.dto.common.IColumnReference;
+import ma.ilias.dbmanagementbe.validation.ExistingColumn;
+import ma.ilias.dbmanagementbe.validation.ValidUniqueChange;
+
+@Data
+@ExistingColumn
+@ValidUniqueChange
+public class UpdateColumnUniqueDto implements IColumnReference {
+    @NotBlank(message = "Schema name is required")
+    private String schemaName;
+
+    @NotBlank(message = "Table name is required")
+    private String tableName;
+
+    @NotBlank(message = "Column name is required")
+    private String columnName;
+
+    @NotNull(message = "Unique value is required")
+    private Boolean isUnique;
+}
---
+++ src/main/java/ma/ilias/dbmanagementbe/validation/ValidUniqueChange.java
@@ -0,0 +1,20 @@
+package ma.ilias.dbmanagementbe.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = ValidUniqueChangeValidator.class)
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidUniqueChange {
+    String message() default "Cannot set column to UNIQUE";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
