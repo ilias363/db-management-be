ea415228077ab816480afbc6e90754466ce63500
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-07-24 12:35:30 +0100

    Add search response dto

---
+++ src/main/java/ma/ilias/dbmanagementbe/record/dto/AdvancedSearchResponseDto.java
@@ -0,0 +1,37 @@
+package ma.ilias.dbmanagementbe.record.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class AdvancedSearchResponseDto {
+    @JsonIgnoreProperties({"schemaName", "tableName"})
+    private List<RecordDto> records;
+
+    private long totalRecords;
+    private long filteredRecords; // Records after filtering but before pagination
+    private int currentPage;
+    private int pageSize;
+    private int totalPages;
+    private String tableName;
+    private String schemaName;
+
+    // Search metadata
+    private boolean hasFilters;
+    private boolean hasGlobalSearch;
+    private boolean hasSort;
+    private boolean isDistinct;
+
+    // Applied filters and sorts for reference
+    private List<FilterCriteriaDto> appliedFilters;
+    private List<SortCriteriaDto> appliedSorts;
+    private String appliedGlobalSearch;
+}
