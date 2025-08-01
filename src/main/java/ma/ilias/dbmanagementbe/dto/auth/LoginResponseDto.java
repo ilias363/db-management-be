0454c280003e3d3ff7f656b67635b2e35d404df9
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-08-01 15:49:43 +0100

    add login response dto

---
+++ src/main/java/ma/ilias/dbmanagementbe/dto/auth/LoginResponseDto.java
@@ -0,0 +1,12 @@
+package ma.ilias.dbmanagementbe.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDto {
+    private String accessToken;
+}
